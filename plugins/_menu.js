366a61f017e4b082fa102b4ca976b457bb35a29e
sks2k6 <108617100+sks2k6@users.noreply.github.com>
2025-05-20 22:40:40 +0530

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -96,3 +96,114 @@ Description: ${i.desc}`);
     }
   }
 );
+
+bot(
+  {
+    pattern: "list",
+    fromMe: isPrivate,
+    desc: "Show All Commands",
+    type: "user",
+    dontAddCommandList: true,
+  },
+  async (message, match, { prefix }) => {
+    const contextInfo = {
+      forwardingScore: 1,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363332700909499@newsletter',
+        newsletterName: BOT_NAME,
+        serverMessageId: -1
+      }
+    };
+    let menu = "Command List\n";
+
+    let cmnd = [];
+    let cmd, desc;
+    plugins.commands.map((command) => {
+      if (command.pattern) {
+        cmd = command.pattern.toString().split(/\W+/)[1];
+      }
+      desc = command.desc || false;
+
+      if (!command.dontAddCommandList && cmd !== undefined) {
+        cmnd.push({ cmd, desc });
+      }
+    });
+    cmnd.sort();
+    cmnd.forEach(({ cmd, desc }, num) => {
+      menu += `\n${num + 1}. ${cmd.trim()}\n`;
+      if (desc) menu += `Use: ${desc}`;
+    });
+    menu += ``;
+  return await message.client.sendMessage(
+      message.jid,
+      {
+        text: menu,
+        contextInfo
+      },
+      {
+        quoted: {
+          key: message.key,
+          message: {
+            conversation: message.text || message.body || ''
+          }
+        }
+      }
+    )
+  }
+);
+bot(
+  {
+    pattern: "ping",
+    fromMe: isPrivate,
+    desc: "To check if the bot is awake",
+    type: "user",
+  },
+  async (message, match) => {
+    const contextInfo = {
+      forwardingScore: 1,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363332700909499@newsletter',
+        newsletterName: BOT_NAME,
+        serverMessageId: -1
+      }
+    };
+
+    const start = Date.now();
+
+    // Simulate some processing work
+    for (let i = 0; i < 1e6; i++) {
+      Math.sqrt(i);
+    }
+
+    const end = Date.now();
+    const speed = end - start;
+
+    await message.client.sendMessage(
+      message.jid,
+      {
+        text: `Bot speed: ${speed} ms`,
+        contextInfo
+      },
+      {
+        quoted: {
+          key: message.key,
+          message: {
+            conversation: message.text || message.body || ''
+          }
+        }
+      }
+    );
+  }
+);
+
+bot({
+  pattern: 'reboot$',
+  fromMe: true,
+  desc: 'Restart the bot',
+  type: 'user'
+}, async (message, client) => {
+  await message.sendMessage(message.jid, "_rebooting_");
+  return require('pm2').restart('index.js');
+});
